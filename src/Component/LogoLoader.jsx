5e7ca7f83d21ee214b89e623c8bb7c0157b29ef0
Gyan <gyanendra.dubey@bayatree.com>
2024-02-04 20:47:32 +0530

    Feat: Animate loader

--- src/Component/LogoLoader.jsx
+++ src/Component/LogoLoader.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import styles from "./Logo.module.css";
 
 export default function LogoLoader() {
   return (
-    <h2 className='logo-loader'>Stylrax</h2>
-  )
+    <div className={styles.container}>
+      <div id="fountainTextG">
+        <div id="fountainTextG_1" class={styles.fountainTextG}>
+          S
+        </div>
+        <div id="fountainTextG_2" class={styles.fountainTextG}>
+          t
+        </div>
+        <div id="fountainTextG_3" class={styles.fountainTextG}>
+          y
+        </div>
+        <div id="fountainTextG_4" class={styles.fountainTextG}>
+          l
+        </div>
+        <div id="fountainTextG_5" class={styles.fountainTextG}>
+          r
+        </div>
+        <div id="fountainTextG_6" class={styles.fountainTextG}>
+          a
+        </div>
+        <div id="fountainTextG_7" class={styles.fountainTextG}>
+          x
+        </div>
+      </div>
+    </div>
+  );
 }
