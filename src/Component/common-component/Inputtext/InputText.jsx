89cef7b82f32dbe53c3e82070f2526281935f79f
shakyaJSChamps <raj@jschamps.com>
2024-04-16 09:20:05 +0530

    working on salonManagement

--- src/Component/common-component/Inputtext/InputText.jsx
+++ src/Component/common-component/Inputtext/InputText.jsx
@@ -2,11 +2,16 @@ import { Field, ErrorMessage } from 'formik'
 import style from '../Inputtext/controls.module.css';
 
 const InputText = props => {
-  const {label, name, ...rest } = props
+  const { label, name,value, ...rest } = props
   return (
     <div className='d-flex flex-column mb-1'>
-      <label htmlFor={name} className={style.bold}>{label}</label> 
-      <Field id={name} className={`${style.control__input} form-control input`} name={name} {...rest} />
+      <label htmlFor={name} className={style.bold}>{label}</label>
+      <Field
+        id={name}
+        className={`${style.control__input} form-control input`}
+        name={name}
+        value={value}
+        {...rest} />
       {/* <ErrorMessage component="div" name={name} className={style.control__input_error} /> */}
     </div>
   )
