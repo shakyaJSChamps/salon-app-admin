eba8d6de3570d3567ab8c5cdac8c60d1a98def4f
shakyaJSChamps <raj@jschamps.com>
2024-05-10 09:28:22 +0530

    resolved Conflicts

--- src/Component/common-component/Inputtext/InputText.jsx
+++ src/Component/common-component/Inputtext/InputText.jsx
@@ -1,19 +1,25 @@
-import { Field, ErrorMessage } from 'formik'
-import style from '../Inputtext/controls.module.css';
+import { Field, ErrorMessage } from "formik";
+import style from "../Inputtext/controls.module.css";
 
-const InputText = props => {
-  const { label, name, ...rest } = props
+const InputText = ({ label, name, ...rest }) => {
+  const today = new Date().toISOString().split('T')[0];
+  const maxDate = name === 'dob' ? { max: today } : {};
+
   return (
-    <div className='d-flex flex-column mb-1'>
-      <label htmlFor={name} className={style.bold}>{label}</label>
+    <div className="d-flex flex-column mb-1">
+      <label htmlFor={name} className={style.bold}>
+        {label}
+      </label>
       <Field
         id={name}
         className="form-control input"
         name={name}
-        {...rest} />
+        {...rest}
+        {...maxDate} 
+      />
       {/* <ErrorMessage component="div" name={name} className={style.control__input_error} /> */}
     </div>
-  )
-}
+  );
+};
 
 export default InputText;
