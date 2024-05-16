2c7b5861a64ef1dd8af8593f666c30a511462baf
shakyaJSChamps <raj@jschamps.com>
2024-05-16 17:59:25 +0530

    created reusable component for single and multipleImages

---
+++ src/Component/common-component/Multipleimageuploader/MultipleImageUploader.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { fileUploaders, updateImage } from '../../../api/account.api';
+import InputFile from '../../../../Controls/InputFile';
+import Notify from "../../../utils/notify"
+
+function MultipleImageUploader({ salonDetail, setImages, imageType, buttonName, name }) {
+
+    const multipleFileSelect = async (files) => {
+        try {
+            files = Array.isArray(files) ? files : [files];
+            const uploadPromises = files.map(async (file) => {
+                const response = await fileUploaders({ fileName: file.name });
+                const requestOptions = {
+                    method: 'PUT',
+                    body: file,
+                    headers: {
+                        'Content-Type': file.type,
+                    }
+                };
+                await fetch(response.data.data.url, requestOptions);
+                const updatedData = {
+                    imageUrl: response.data.data.path, 
+                    imageType: imageType,
+                    thumbnailUrl: '',
+                };
+                const res = await updateImage(updatedData, salonDetail.id);
+                return { id: res.data.data.id, url: response.data.data.path }; 
+            });
+            const uploadedImages = await Promise.all(uploadPromises);
+            setImages(prevImages => [...prevImages, ...uploadedImages]);
+
+            Notify.success('Images uploaded successfully.');
+        } catch (error) {
+            console.error('Error uploading images:', error);
+            Notify.error('Image upload failed. Please try again.');
+        }
+    };
+    return (
+        <div>
+            <InputFile
+                name={name}
+                onFileSelect={(files) => multipleFileSelect(files)}
+                buttonName={buttonName}
+                multiple
+            />
+        </div>
+
+    )
+}
+
+export default MultipleImageUploader
--- src/Component/salonManagement/SalonGallery/SalonGallery.jsx
+++ src/Component/salonManagement/SalonGallery/SalonGallery.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import InputFile from '../../../../Controls/InputFile';
-import { fileUploaders, updateImage, deleteImage } from '../../../api/account.api';
+import { deleteImage } from '../../../api/account.api';
 import Notify from "../../../utils/notify";
 import styles from "../SalonGallery/Salongallery.module.css";
 import Swal from "sweetalert2";
+import ImageUpdate from '../../common-component/Imageupdate/ImageUpdate';
+import MultipleImageUploader from '../../common-component/Multipleimageuploader/MultipleImageUploader';
 
 function SalonGallery({ salonDetail, bannerImages, gallaryImages }) {
     const [mainGateImageUrl, setMainGateImageUrl] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
     const [bannerImage, setBannerImage] = useState([]);
     const [gallaryImage, setGallaryImage] = useState([]);
 
@@ -19,70 +19,6 @@ function SalonGallery({ salonDetail, bannerImages, gallaryImages }) {
         }
     }, [gallaryImages, bannerImages]);
 
-    const handleOnFileSelect = async (file) => {
-        // Indicate upload in progress
-        setIsLoading(true);
-        try {
-            const response = await fileUploaders({ fileName: file.name });
-            const requestOptions = {
-                method: 'PUT',
-                body: file,
-                headers: {
-                    'Content-Type': file.type,
-                },
-            };
-
-            await fetch(response.data.data.url, requestOptions);
-            const updatedData = {
-                imageUrl: response.data.data.path, // Use uploaded image URL
-                imageType: 'MainGate',
-                thumbnailUrl: '',
-            };
-
-            const updateResponse = await updateImage(updatedData, salonDetail.id);
-            setMainGateImageUrl(updateResponse.data.data.imageUrl); // Update state with saved URL
-            Notify.success(response.data.message);
-        } catch (error) {
-            console.error('Error uploading image:', error);
-            Notify.error('Image upload failed. Please try again.'); // Inform user
-        } finally {
-            setIsLoading(false); // Reset loading state
-        }
-    };
-
-    const gallaryFileSelect = async (files, section) => {
-        try {
-            files = Array.isArray(files) ? files : [files];
-            const uploadPromises = files.map(async (file) => {
-                const response = await fileUploaders({ fileName: file.name });
-                const requestOptions = {
-                    method: 'PUT',
-                    body: file,
-                    headers: {
-                        'Content-Type': file.type,
-                    }
-                };
-                await fetch(response.data.data.url, requestOptions);
-                const updatedData = {
-                    imageUrl: response.data.data.path, // Use uploaded image URL
-                    imageType: section === 'banner' ? 'Banner' : 'Gallery',
-                    thumbnailUrl: '',
-                };
-                const res = await updateImage(updatedData, salonDetail.id);
-                return { id: res.data.data.id, url: response.data.data.path }; // Return object with id and URL
-            });
-            const uploadedImages = await Promise.all(uploadPromises);
-            if (section === 'banner') {
-                setBannerImage([...bannerImage, ...uploadedImages]);
-            } else if (section === 'gallery') {
-                setGallaryImage([...gallaryImage, ...uploadedImages]);
-            }
-            Notify.success('Images uploaded successfully.');
-        } catch (error) {
-            console.error('Error uploading images:', error);
-            Notify.error('Image upload failed. Please try again.'); 
-        }
-    };
 
     const removeImage = async (id, section) => {
         try {
@@ -120,15 +56,14 @@ function SalonGallery({ salonDetail, bannerImages, gallaryImages }) {
             <div className='d-flex justify-content-between'>
                 <h4>Salon Gallery</h4>
                 <div className='d-flex flex-row gap-1'>
-                    <InputFile
+                    <ImageUpdate
+                        salonDetail={salonDetail}
+                        setImageUrl={setMainGateImageUrl}
                         name="mainGateImageUrl"
-                        onFileSelect={handleOnFileSelect}
-                        buttonName={isLoading ? 'Uploading...' : 'Update'} // Display loading state
-                        disabled={isLoading} // Disable input while uploading
-                    />
+                        imageType="Maingate" />
                 </div>
             </div>
-            {salonDetail && ( 
+            {salonDetail && (
                 <div>
                     <h6>Main Gate Image</h6>
                     <img src={mainGateImageUrl == null ? salonDetail.mainGateImageUrl : mainGateImageUrl} style={{ height: '150px', width: '150px' }} alt="Main Gate" />
@@ -141,54 +76,56 @@ function SalonGallery({ salonDetail, bannerImages, gallaryImages }) {
             <div>
                 <div className='d-flex justify-content-between align-items-center'>
                     <h6>Banner Images</h6>
-                    <InputFile
+                    <MultipleImageUploader
+                        salonDetail={salonDetail}
+                        setImages={setBannerImage}
+                        imageType="Banner"
+                        buttonName="Add"
                         name="bannerImages"
-                        onFileSelect={(files) => gallaryFileSelect(files, 'banner')}
-                        buttonName="Add"
-                        multiple 
                     />
                 </div>
-            </div>
-            {bannerImage.length > 0 && (
-                <div className='d-flex flex-row flex-wrap gap-2'>
-                    {bannerImage.map((image, index) => (
-                        <div key={index} className={styles.imageContainer}>
-                            <img src={image.url} style={{ height: '150px', width: '150px' }} alt={`Banner Image ${index}`} />
-                            <button type="button" className={styles.deleteButton} onClick={() => removeImage(image.id, 'banner')}>Remove</button>
-                        </div>
-                    ))}
+                {bannerImage.length > 0 && (
+                    <div className='d-flex flex-row flex-wrap gap-2'>
+                        {bannerImage.map((image, index) => (
+                            <div key={index} className={styles.imageContainer}>
+                                <img src={image.url} style={{ height: '150px', width: '150px' }} alt={`Banner Image ${index}`} />
+                                <button type="button" className={styles.deleteButton} onClick={() => removeImage(image.id, 'banner')}>Remove</button>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                <hr />
                 </div>
-            )}
 
-            <hr />
-
-            {/* Gallery Images */}
-            <div>
-                <div className='d-flex justify-content-between '>
-                    <h6>Gallery Images</h6>
-                    <InputFile
-                        name="galleryImages"
-                        onFileSelect={(files) => gallaryFileSelect(files, 'gallery')}
-                        buttonName="Add"
-                        multiple 
-                    />
-                </div>
-            </div>
-            {gallaryImage.length > 0 && (
-                <div className='d-flex flex-row flex-wrap gap-2 mb-3'>
-                    {gallaryImage.map((image, index) => (
-                        <div key={index} className={styles.imageContainer}>
-                            <img src={image.url} style={{ height: '150px', width: '150px' }} alt={`Gallery Image ${index}`} />
-                            <button type="button" className={styles.deleteButton} onClick={() => removeImage(image.id, 'gallery')}>Remove</button>
+                {/* Gallery Images */}
+                <div className='mb-3'>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <h6>Gallery Images</h6>
+                        <MultipleImageUploader
+                            salonDetail={salonDetail}
+                            setImages={setGallaryImage}
+                            imageType="Gallery"
+                            buttonName="Add"
+                            name="galleryImages"
+                        />
+                    </div>
+                    {bannerImage.length > 0 && (
+                        <div className='d-flex flex-row flex-wrap gap-2'>
+                            {gallaryImage.map((image, index) => (
+                                <div key={index} className={styles.imageContainer}>
+                                    <img src={image.url} style={{ height: '150px', width: '150px' }} alt={`Gallery Image ${index}`} />
+                                    <button type="button" className={styles.deleteButton} onClick={() => removeImage(image.id, 'gallery')}>Remove</button>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    )}
                 </div>
-            )}
-        </>
-    );
+
+            </>
+            );
 }
 
-export default SalonGallery;
+            export default SalonGallery;
 
 
 
